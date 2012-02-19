f6f10659000edf49831906b8618adb79f169093c
unknown <shrutic@MALUGUS-MSFT.redmond.corp.microsoft.com>
2012-02-18 20:37:11 -0800

    temp

--- Buzz/Service.cs
+++ Buzz/Service.cs
@@ -146,5 +146,7 @@ namespace Buzz
             request = null;
             streamResponse = null;
         }
+        public delegate void OnResultDelegate(IAsyncResult result);
+        public event OnResultDelegate OnResult;
     }
 }
